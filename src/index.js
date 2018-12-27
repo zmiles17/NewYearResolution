c36536370f93c6935ec882f106ff231a05b66ce3
zmiles17 <zgm_striker17@hotmail.com>
2018-12-26 21:17:56 -0500

    FontAwesome Imported

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight, faBookOpen, faUtensils, faBicycle, faUsers } from '@fortawesome/free-solid-svg-icons';
+import './reset.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+library.add(faChevronLeft, faChevronRight, faBookOpen, faUtensils, faBicycle, faUsers)
+
+const Calendar = props => (
+    <React.Fragment>
+
+        <h1>
+            <FontAwesomeIcon icon="chevron-left" />
+            {props.currentMonth}
+            <FontAwesomeIcon icon="chevron-right" />
+        </h1>
+        <div className='Month-Container'>
+            <div className='Day-Container'>
+
+            </div>
+        </div>
+    </React.Fragment>
+)
+
+class App extends React.Component {
+    state = {
+        currentMonth: 'December',
+    }
+    
+    render() {
+        return (
+            <div className='App'>
+                <main>
+                    <Calendar currentMonth={this.state.currentMonth} />
+                </main>
+                <aside>
+                    {/* <Statistic /> */}
+                </aside>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
